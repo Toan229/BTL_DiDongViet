0808b1ab5788c7532d267aebb2d39a55514d3ea3
tahn-0102 <ongthoxunu@gmail.com>
2021-12-23 22:17:39 +0700

    done

--- BTL_DiDongViet/Models/News.cs
+++ BTL_DiDongViet/Models/News.cs
@@ -1,4 +1,4 @@
-namespace BTL_DiDongViet.Models
+﻿namespace BTL_DiDongViet.Models
 {
     using System;
     using System.Collections.Generic;
@@ -11,17 +11,21 @@ namespace BTL_DiDongViet.Models
         public long ID { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Tiêu đề")]
         public string Title { get; set; }
 
         [Column(TypeName = "ntext")]
+        [Display(Name = "Nội dung")]
         public string NewContent { get; set; }
 
         [StringLength(10)]
+        [Display(Name = "Ảnh")]
         public string Image { get; set; }
 
         [StringLength(500)]
+        [Display(Name = "Mô tả")]
         public string Description { get; set; }
-
+        [Display(Name = "Ngày tạo")]
         public DateTime? CreatedDate { get; set; }
     }
 }
