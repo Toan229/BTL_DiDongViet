e8e69783cb142c7c546f5bacda1c1f9c9d28ec41
ngiem2805 <nghiemle2000thcstantien@gmail.com>
2021-12-23 16:42:10 +0700

    create order branch

---
+++ BTL_DiDongViet/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTL_DiDongViet.Models;
+
+namespace BTL_DiDongViet.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private DBDiDongViet db = new DBDiDongViet();
+
+        // GET: Orders
+        public ActionResult Index(/*int id*/)
+        {
+            /*var orders = db.Orders.Include(o => o.ID);
+            return View(orders.ToList());*/
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
