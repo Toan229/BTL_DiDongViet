b9d9ab0880274e4fc881631511f3122f25a312c5
Toan Pham <canhtoan229@gmail.com>
2021-12-25 21:17:28 +0700

    ảnh và category

---
+++ BTL_DiDongViet/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTL_DiDongViet.Models
+{
+    public class HomeViewModel
+    {
+        public List<Products> Product { get; set; }
+        public List<ProductCategory> Category { get; set; }
+    }
+}
