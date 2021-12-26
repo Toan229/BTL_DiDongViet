9ca97c61d40d44bf8c9cee9ff1b10b7ab2342e05
tahn-0102 <ongthoxunu@gmail.com>
2021-12-26 20:18:16 +0700

    ProductSP

---
+++ BTL_DiDongViet/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using BTL_DiDongViet.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList.Mvc;
+
+namespace BTL_DiDongViet.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public DBDiDongViet db = new DBDiDongViet();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail(long id)
+        {
+            var model = db.Products.SingleOrDefault(x => x.ID == id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Lst = db.Products.Where(x => x.ID != id).ToList();
+            return View(model);
+        }
+
+    }
+}
--- BTL_DiDongViet/Models/Products.cs
+++ BTL_DiDongViet/Models/Products.cs
@@ -1,4 +1,4 @@
-namespace BTL_DiDongViet.Models
+﻿namespace BTL_DiDongViet.Models
 {
     using System;
     using System.Collections.Generic;
@@ -15,40 +15,59 @@ namespace BTL_DiDongViet.Models
             OrderDetail = new HashSet<OrderDetail>();
         }
 
+        [Display(Name = "Mã sản phẩm")]
         public long ID { get; set; }
 
         [Required]
         [StringLength(250)]
+        [Display(Name = "Tên sản phấm")]
+
         public string ProductName { get; set; }
 
         public long? CategoryID { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Số lượng còn")]
+
         public string Storage { get; set; }
 
+
         [Column(TypeName = "money")]
+        [Display(Name = "Giá sản phẩm")]
+
         public decimal Price { get; set; }
 
         [Required]
         [StringLength(50)]
         public string Color { get; set; }
+        [Display(Name = "Giảm giá")]
 
         public decimal? Sale { get; set; }
+        [Display(Name = "Số lượng")]
 
         public long Quantity { get; set; }
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "Ảnh sản phẩm")]
+
         public string Image { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "Mô tả")]
+
         public string Description { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "Bộ phụ kiện")]
+
         public string Sets { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "Bảo hành")]
+
         public string Insurance { get; set; }
+        [Display(Name = "Đã bán")]
 
         public long? NumberOfPurchases { get; set; }
 
